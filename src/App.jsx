e395cf9ed0bf29aa9905e8de2cef184b57a22ef2
Uberpurplecat <141165386+Uberpurplecat@users.noreply.github.com>
2023-09-17 20:33:24 -0500

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import './App.css'
 import {puppyList} from './data.js'
-import { useState } from 'react'
+import {useState} from 'react'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log("puppyList: ", puppies);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);  
   return (
-    <>
       <div className="card">
         { 
         puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+          return (
+            <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+            </p>
+            );
         })
          }
+         {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+         )} 
       </div>
-    </>
   )
 }
 
